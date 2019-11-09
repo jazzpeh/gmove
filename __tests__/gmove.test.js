11984be79b20356ace80457598f93c1d2298b182
Jazz Peh <jazzpehz@gmail.com>
2019-11-09 23:26:42 +0800

    Started unit testing

---
+++ __tests__/gmove.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+import {GMove} from '../src';
+
+describe('GMove', () => {
+  describe('execute()', () => {
+    it('should delay the execution based on the passed `delay` parameter.', () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
